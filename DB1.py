ad375fbd3cc196726db9686ccc1b6ab04f8cf26f
leehun14 <nd9819@naver.com>
2024-06-18 16:49:52 +0900

    데이터베이스 기본

---
+++ DB1.py
@@ -0,0 +1,14 @@
+#데이터베이스
+import sqlite3
+
+#연결객체 리턴
+con = sqlite3.connect(":memory:")
+
+#구문을 실행할 커서 객체 리턴
+cur = con.cursor()
+
+#테이블 생성
+cur.execute("create table PhoneBook (Name text, PhoneNum text);")
+
+#1건 입력
+cur.execute("insert into PhoneBook values ('derick','010-222');")
