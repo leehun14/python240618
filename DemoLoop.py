44958af8890589abfe4048cdfd3434a9dd949c59
leehun14 <nd9819@naver.com>
2024-06-18 09:43:40 +0900

    클래스 연습1

--- DemoLoop.py
+++ DemoLoop.py
@@ -1,14 +1,37 @@
 #DemoLoop.py
 
-value = 5
-while value >0:
-    print(value)
-    value -=1
-
-lst = ["python", 100, 3.14]
-for item in lst:
-    print(item)
-
 fruits = {"apple":100, "kiwi":200}
 for item in fruits.items():
+    print(item)
+
+print("---range함수----")
+print(list(range(10)))
+print(list(range(2000,2025)))
+print(list(range(1,32)))
+
+print("---리스트 컴프리핸션---")
+lst = list(range(1,11))
+print([i**2 for i in lst if 1> 5])
+tp = ("apple","kiwi","banana")
+print(([len(i) for i in tp]))
+
+
+print("---필터링---")
+lst = [10,25,30]
+itemL = filter(None, lst)
+for item in itemL:
+    print(item)
+
+#함수
+def getBiggerThan20(i):
+    return i > 20
+
+print("---필터링 함수 사용")
+itemL = filter(getBiggerThan20, lst)
+for item in itemL:
+    print(item)
+
+print("----람다 함수 사용----")
+itemL = filter(lambda x:x>20,lst)
+for item in itemL:
     print(item)
---
+++ class1.py
@@ -0,0 +1,12 @@
+#1) 클래스 정의
+
+class Person:
+    #초기화 매소드
+    def __init__(self):
+        self.name = "default name"
+    def print(self):
+        print("My name is {0}".format(self.name))
+#2) 인스턴스 생성
+p1 = Person()
+#3) 메소드 호출
+p1.print()
