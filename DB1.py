eca215f209c68b11a618a002321f716f8ce95023
leehun14 <nd9819@naver.com>
2024-06-19 09:15:51 +0900

    데이터베이스2

--- DB1.py
+++ DB1.py
@@ -12,3 +12,13 @@ cur.execute("create table PhoneBook (Name text, PhoneNum text);")
 
 #1건 입력
 cur.execute("insert into PhoneBook values ('derick','010-222');")
+
+#입력 파라미터 처리
+name = "홍길동"
+phonenumber = "010-123"
+cur.execute("insert into PhoneBook values (?, ?);", (name, phonenumber))
+
+#조회를 실행
+cur.execute("select * from PhoneBook;")
+for row in cur:
+    print(row[0], row[1])
